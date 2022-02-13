86d6593b0011b109467ecc2eee3364040dac05aa
Ruby Wai <rubyland.myanmar.mg@gmail.com>
2022-02-13 19:43:14 +0630

    deploy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e9eb58db72d407be27e9fa052224c304",
-"index.html": "f723bfa63190d7882952fa9f7c056046",
-"/": "f723bfa63190d7882952fa9f7c056046",
-"main.dart.js": "dffdc1a84a474ae9fe44bfd20e7a93fc",
+"index.html": "44d344d8a98e298d0fffffd019894ca7",
+"/": "44d344d8a98e298d0fffffd019894ca7",
+"main.dart.js": "235ee233a98eaf4a20dd682ad1512571",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
